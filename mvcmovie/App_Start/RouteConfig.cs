1285b640f927355c87e8a34b7341b5badb91e87a
tangjiangxu <2287280228@qq.com>
2019-11-18 19:18:02 +0800

    .net mvc5.0 第一次初始化

---
+++ mvcmovie/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace mvcmovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                // 默认的控制器为HomeController, 默认的方法为index
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ mvcmovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcmovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            //return "This is my <b>defualt</b> aciton";
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome
+        public ActionResult Welcome(int Id, int numTimes = 1)
+        {
+            ViewBag.Id = "hello" + Id;
+            ViewBag.NumTimes = numTimes;
+
+            //return HttpUtility.HtmlEncode("Hello " + Id + ",Numtimes is: " + numTimes);
+            return View();
+        }
+    }
+}
