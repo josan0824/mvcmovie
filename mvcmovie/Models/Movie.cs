4b27be3f9301732e3d5fe20610b352636e407ff7
tangjiangxu <2287280228@qq.com>
2019-11-22 14:07:15 +0800

    添加movie实体类，添加ef

---
+++ mvcmovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace mvcmovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { set; get; }
+
+        public string Genre { set; get; }
+
+        public decimal Price { set; get; }
+    }
+
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Moives { get; set; }
+    }
+}
